1a60b90834e07bf1e33056cd9e45db77765acce7
Deni Simon <denimallel@gmail.com>
2020-08-09 20:26:17 +0100

    Added initial Spring Boot project

---
+++ src/main/java/com/deni/web/UserProfileManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.deni.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserProfileManagementApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserProfileManagementApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/deni/web/UserProfileManagementApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deni.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserProfileManagementApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
